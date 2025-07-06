76fa5beeb54ef7e8951e0e57eece6cb6c13035f1
nupur-bhatt <nupurbhatt999@gmail.com>
2025-07-06 14:12:31 -0300

    Added /submit endpoint

--- index.js
+++ index.js
@@ -5,15 +5,27 @@ const app = express();
 const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+var postArray = [];
+var mainTitle = "dev.blog";
 
 app.get("/", (req,res)=>{
     var date = new Date();
     var year = date.getFullYear();
-    res.render("index.ejs", res.locals ={
-        title : "dev.blog",
-        currentYear: year
+    res.render("index.ejs", res.locals = {
+        title : mainTitle,
+        currentYear: year,
+        arraysBlog : postArray 
     });
 }); 
+
+app.post("/submit", (req,res)=>{
+    postArray.push({
+        author : req.body.author,
+        blogTitle : req.body.blogTitle,
+        content : req.body.content,
+    });
+    res.redirect('/');
+});
 
 app.listen(port, ()=>{
     console.log(`server running on port ${port}`);
