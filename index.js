4ae3ecf6dca84076a572af4c6080759ec5e172be
nupur-bhatt <nupurbhatt999@gmail.com>
2025-07-10 20:48:03 -0300

    changed port for deployment

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 import methodOverride from 'method-override';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 var postsArray = [];
 var mainTitle = "dev.blog";
 
